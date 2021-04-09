e8db2249a65e72a3c18b8e9b16e2fea9fff1eeff
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-09 14:27:03 -0400

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Brie New on 4/9/21.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
