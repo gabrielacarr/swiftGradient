536fe870c79f969dc7022f69edde332414b0415d
brie <24256452+gabrielacarr@users.noreply.github.com>
2021-04-09 15:55:38 -0400

    Change: gradient style for heading

--- demo/ContentView.swift
+++ demo/ContentView.swift
@@ -7,15 +7,36 @@
 
 import SwiftUI
 
-struct ContentView: View {
+
+
+struct GradientText: View {
+    let text: Text
+    let gradient: LinearGradient
+    
+    init(text: Text, gradient: LinearGradient? = nil) {
+        self.text = text
+        self.gradient = gradient ?? LinearGradient(gradient: Gradient(colors: [.pink, .yellow, .pink, .orange]), startPoint: .leading, endPoint: .trailing)
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        text
+        .foregroundColor(.clear)
+        .overlay(
+            gradient
+            .mask(text))
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            GradientText(text: Text("brie")
+                .font(.system(size: 40, weight: .bold, design: .rounded)))
+
+                          Text("is short for ga").font(.system(size: 33)).fontWeight(.thin).foregroundColor(.orange) +
+                          Text("brie").font(.system(size: 33)).fontWeight(.bold).foregroundColor(.yellow) +
+                          Text("la").font(.system(size: 33)).fontWeight(.thin).foregroundColor(.orange)
+        }
     }
 }
+
